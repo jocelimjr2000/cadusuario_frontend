64aa30f7966fed2ea01cc49495a6e9fc95842a14
Gustavo-RC <gu171994@gmail.com>
2021-05-01 18:57:20 -0300

    m

--- lib/bloc/cadastro_pesquisar_usuario_bloc.dart
+++ lib/bloc/cadastro_pesquisar_usuario_bloc.dart
@@ -1,10 +1,8 @@
 import 'package:cadusuario_frontend/model/cadastro_usuario_model.dart';
 import 'package:cadusuario_frontend/service/cadastro_pesquisar_usuario_service.dart';
-import 'package:cadusuario_frontend/service/cadastro_usuario_service.dart';
 import 'package:rxdart/rxdart.dart';
 
 class CadastroPesquisarUsuarioBloc {
-
   final BehaviorSubject _controller = BehaviorSubject();
 
   Sink get dataIn => _controller.sink;
@@ -12,7 +10,7 @@ class CadastroPesquisarUsuarioBloc {
   Stream<CadastroUsuarioModel> get dataOut => _controller.stream.asyncMap(
         (_) {
           Future<CadastroUsuarioModel> data = getCadastroPesquisarUsuario('20123123197'); //todo falta ver como que passa o parametro cpf para filtrar, fazer tela de input cpf e tela de exibição da busca, chamar na tela inicial
-          return data;
+          return data; //todo ver pra rodar no web com aplicação local dio error
         },
       );
 
@@ -23,20 +21,6 @@ class CadastroPesquisarUsuarioBloc {
   Future<Null> load() async {
     dataIn.add(null);
   }
-
-  Future<Null> save(CadastroUsuarioModel cadastroUsuarioModel) async {
-    await postCadastroUsuario(cadastroUsuarioModel);
-  }
-
-  Future<Null> update(CadastroUsuarioModel cadastroUsuarioModel) async {
-    await putCadastroUsuario(cadastroUsuarioModel);
-  }
-
-  // Future<Null> delete(CadastroUsuarioModel cadastroUsuarioModel) async {
-  //   await deleteCadastroUsuario(cadastroUsuarioModel);
-  //
-  //   reLoad();
-  // }
 
   void dispose() {
     _controller.close();
